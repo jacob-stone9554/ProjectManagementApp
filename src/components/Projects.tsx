e2efbdeeabe1abdef6cc00f197f6cf5057acf5ae
Jacob Stone <jumboshrimp9554@gmail.com>
2025-08-26 23:09:49 -0400

    Set up the initial project/task list areas

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,8 +1,14 @@
 function Projects() {
     return(
-        <div className="bg-white w-5/6 h-5/6 rounded-2xl flex flex-col justify-center items-center">
-            <h1>Project List</h1>
-            <p>This is where the projects will live.</p>
+        <div className="bg-white w-5/6 h-5/6 rounded-2xl flex flex-row justify-evenly items-center">
+            <div className="flex flex-col items-center justify-start pt-10 h-full">
+                <h1 className="text-2xl">List of projects will go here</h1>
+                <p>Clicking on the projects will open a list of tasks on the right</p>
+            </div>
+            <div className="flex flex-col items-center justify-start pt-10 h-full">
+                <h1 className="text-2xl">List of tasks will go here</h1>
+                <p>This area will be populated when a project is selected</p>
+            </div>
         </div>
     )
 }
